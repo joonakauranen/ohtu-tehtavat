044ef8000cca9968987faee2528d2429aaea56a7
Joona <joona.kauranen@helsinki.fi>
2021-11-21 21:34:54 +0200

    t6

--- viikko3/login-robot/src/services/user_service.py
+++ viikko3/login-robot/src/services/user_service.py
@@ -1,5 +1,5 @@
 from entities.user import User
-
+import re
 
 class UserInputError(Exception):
     pass
@@ -26,12 +26,25 @@ class UserService:
 
     def create_user(self, username, password):
         self.validate(username, password)
+        self.check_user(username)
+        self.check_password(password)
 
         user = self._user_repository.create(
             User(username, password)
         )
 
         return user
+
+    def check_user(self, username):
+        if not re.search("^[a-z]+$", username) or len(username) < 3:
+            raise UserInputError("Invalid username")
+
+    
+    def check_password(self, password):
+        if not re.search("[^a-z]", password) or len(password) < 8:
+            raise UserInputError("Invalid password")
+         
+         
 
     def validate(self, username, password):
         if not username or not password:
