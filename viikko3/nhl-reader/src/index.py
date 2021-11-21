f4b454ac7d43de60122327ca1b3ef605ce006723
Joona <joona.kauranen@helsinki.fi>
2021-11-21 15:25:49 +0200

    nhl

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -5,19 +5,30 @@ def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
 
     players = []
 
     for player_dict in response:
         player = Player(
-            player_dict['name']
+            player_dict['name'],
+            player_dict['nationality'],
+            player_dict['team'],
+            player_dict['goals'],
+            player_dict['assists']
         )
 
-        players.append(player)
+        if player.nationality == "FIN":
+
+            players.append(player)
 
     print("Oliot:")
 
+    players.sort(key=lambda player: player.points, reverse=True)
+
     for player in players:
         print(player)
+
+if __name__ == "__main__":
+    main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, team, goals, assists):
         self.name = name
+        self.nationality = nationality
+        self.team = team
+        self.goals = goals
+        self.assists = assists
+        self.points = int(goals) + int(assists)
     
     def __str__(self):
-        return self.name
+        return f"{self.name:20}" + "    " + self.team + " " + f"{str(self.goals):2}" + " + " + f"{str(self.assists):2}" + " = " + f"{str(self.points):2}"
